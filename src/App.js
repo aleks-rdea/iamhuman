d74bfdf8ed4554084744a8a9dd6539b6ddc1781b
Aleks Witko <aleks@rdea.com>
2020-03-25 15:10:51 +1100

    Landing page start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const createHumanCards = () => {
+    let humans = [];
+
+    for (let j = 0; j < 1002; j++) {
+      humans.push(
+        <li>
+          <div className="face"></div>
+          <dl>
+            <dt>Unknown Human</dt>
+            <dd>Wuhan, China</dd>
+          </dl>
+        </li>
+      );
+    }
+
+    return humans;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <h2>Coronavirus Pandemic</h2>
+      <h1>I am human.</h1>
+      <section className="columns two">
+        <p className="large column">
+          A single person lost is a tragedy. A thousand people lost is
+          statistics. Help us pay tribute and show dignity to those we've lost
+          by remembering their name.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="column summary">
+          <ul className="actions">
+            <li>
+              <a className="button">Plant a flower</a>
+            </li>
+            <li>
+              <a className="button secondary">Name someone</a>
+            </li>
+          </ul>
+          <dl>
+            <dt>Started 19 December 2019</dt>
+            <dd>in Wuhan, China</dd>
+          </dl>
+          <dl>
+            <dt>Humans lost so far</dt>
+            <dd>353,294</dd>
+          </dl>
+        </div>
+      </section>
+
+      <ul className="cards">{createHumanCards()}</ul>
+      {/* <a href="mailto:volunteer@iamhuman.vip">Volunteers</a> */}
     </div>
   );
-}
+};
 
 export default App;
